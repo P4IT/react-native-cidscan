15e2e8562ee8493aa611f7d9f412030ecab7b136
Uwe Hoppe <u.hoppe@p4it.com>
2021-08-11 21:39:44 +0200

    version_1.0.82

--- android/src/main/java/com/reactlibrary/CIDScanView.java
+++ android/src/main/java/com/reactlibrary/CIDScanView.java
@@ -1,12 +1,26 @@
 package com.reactlibrary;
 
+import android.graphics.Point;
 import android.view.View;
+import android.view.ViewGroup;
+
 import androidx.annotation.NonNull;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
 import com.facebook.react.uimanager.SimpleViewManager;
 import com.facebook.react.uimanager.ThemedReactContext;
+import com.facebook.react.uimanager.ViewGroupManager;
 
-public class CIDScanView extends SimpleViewManager<View> {
+import app.captureid.captureidlibrary.BoundedLayout;
+import app.captureid.captureidlibrary.CaptureID;
+import app.captureid.captureidlibrary.SimpleScanner;
+import app.captureid.captureidlibrary.result.ResultListener;
+import app.captureid.captureidlibrary.result.ResultObject;
+
+public class CIDScanView extends ViewGroupManager<ConstraintLayout> {
     private static final String REACT_CLASS = "CIDScanView";
+
+    private CaptureID _captureID;
 
     @NonNull
     @Override
@@ -16,8 +30,30 @@ public class CIDScanView extends SimpleViewManager<View> {
 
     @NonNull
     @Override
-    protected View createViewInstance(@NonNull ThemedReactContext reactContext) {
-        View view = new View(reactContext);
-        return view;
+    protected ConstraintLayout createViewInstance(@NonNull ThemedReactContext reactContext) {
+        ConstraintLayout layout = new ConstraintLayout(reactContext);
+        layout.setLayoutParams(new ConstraintLayout.LayoutParams(ConstraintLayout.LayoutParams.MATCH_CONSTRAINT, ConstraintLayout.LayoutParams.MATCH_CONSTRAINT));
+        SimpleScanner _scanner = SimpleScanner.getSharedObject(reactContext);
+        Point displaySize = new Point(0,0);
+        reactContext.getCurrentActivity().getWindowManager().getDefaultDisplay().getSize(displaySize);
+        _scanner.setDisplaySize(displaySize);
+        _scanner.startScanner(layout);
+        return layout;
+    }
+
+    @Override
+    public void onDropViewInstance(@NonNull ConstraintLayout view) {
+        for(int i = 0; i < view.getChildCount(); i++) {
+            if(view.getChildAt(i) instanceof BoundedLayout) {
+                ((BoundedLayout) view.getChildAt(0)).removeAllViews();
+            }
+        }
+        view.removeAllViews();
+        super.onDropViewInstance(view);
+    }
+
+    @Override
+    protected void finalize() throws Throwable {
+        super.finalize();
     }
 }
