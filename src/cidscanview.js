2b9309a9d7a08e5f7fb9434b56b260791bd02b44
Uwe Hoppe <u.hoppe@p4it.com>
2021-10-04 15:23:18 +0200

    version 1.0.90

--- src/cidscanview.js
+++ src/cidscanview.js
@@ -2,11 +2,11 @@ import PropTypes from 'prop-types';
 import { requireNativeComponent, ViewPropTypes } from 'react-native';
 
 var viewProps = {
-  name: 'CIDScanView',
+  name: 'RNCIDScanView',
   propTypes: {
     config: PropTypes.object,
     ...ViewPropTypes,
   },
 };
 
-module.exports = requireNativeComponent('CIDScanView', viewProps);
+module.exports = requireNativeComponent('RNCIDScanView', viewProps);
